6176b074606568b3866dffaa28e599114059385b
MistrMouse <MistrMouseMail@gmail.com>
2026-02-17 11:58:32 -0500

    Restructure and removal of old files as well as adding test reports generated

--- src/main/java/com/ransom/d2r/D2RModHelper.java
+++
@@ -1,9 +0,0 @@
-package com.ransom.d2r;
-
-import javax.swing.*;
-
-public class D2RModHelper {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(D2RExtractionUI::startExtraction);
-    }
-}
